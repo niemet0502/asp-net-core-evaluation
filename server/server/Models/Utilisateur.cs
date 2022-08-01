cc114daadfa689e2a8d4fd8123cf1068308addef
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-08-01 18:14:14 +0000

    create Utilisateur models class

---
+++ server/server/Models/Utilisateur.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server.Models
+{
+    public class Utilisateur
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        public string Email { set; get; }
+
+        public string password { set; get; }
+
+        public ICollection<Token> Tokens { set; get; }
+    }
+}
