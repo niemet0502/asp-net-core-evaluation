46e7db1bbb1cb38a5461c62c2ca8adf44c4f4938
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-08-01 18:14:28 +0000

    create the db context

---
+++ server/server/Models/ExamenContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace server.Models
+{
+    public class ExamenContext: DbContext
+    {
+        public ExamenContext(DbContextOptions<ExamenContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Utilisateur> Utilisateurs { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Employe> Employes { get; set; }
+        public DbSet<Token> Tokens { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Utilisateur>().ToTable("Utilisateur");
+            modelBuilder.Entity<Task>().ToTable("Task");
+            modelBuilder.Entity<Employe>().ToTable("Employe");
+            modelBuilder.Entity<Token>().ToTable("Token");
+        }
+
+    }
+}
