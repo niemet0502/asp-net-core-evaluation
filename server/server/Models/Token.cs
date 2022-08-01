a665a345a810eb7cd96dd78ea6b1320f083d0ba2
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-08-01 18:13:51 +0000

    create Token models class

---
+++ server/server/Models/Token.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server.Models
+{
+    public class Token
+    {
+        public int ID { get; set; }
+        public string Content { get; set; }
+        public string ExpirationDate { get; set; }
+
+        public int UtilisateurID { get; set; }
+    }
+}
