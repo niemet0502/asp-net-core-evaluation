e9ec7a58f7156acc4ca88eafef635a100ebbe79c
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-08-01 18:13:40 +0000

    create Task models class

---
+++ server/server/Models/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server.Models
+{
+    public class Task
+    {
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Status { get; set; }
+
+        public int EmployeID { get; set; }
+    }
+}
