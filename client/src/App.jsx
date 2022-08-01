9953c4299e711508d550906dc7ed3ae434c95f09
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-08-01 21:01:20 +0000

    change the App file extension

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "./App.css";
+import SignIn from "./pages/Auth/SignIn";
+function App() {
+  return (
+    <div className="App">
+      <SignIn />
+    </div>
+  );
+}
+
+export default App;
