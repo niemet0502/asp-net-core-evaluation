47d03b444ede38fa04b30f1441af12c140f50625
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-08-02 21:47:21 +0000

    create EmployeController and CRUD MEthods

---
+++ server/server/Controllers/EmployesController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using server.Models;
+
+namespace server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployesController : ControllerBase
+    {
+        private readonly ExamenContext _context;
+
+        public EmployesController(ExamenContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Employes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employe>>> GetEmployes()
+        {
+            return await _context.Employes.Include(t => t.Tasks).ToListAsync();
+        }
+
+        // GET: api/Employes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employe>> GetEmploye(int id)
+        {
+            var employe = await _context.Employes.FindAsync(id);
+
+            if (employe == null)
+            {
+                return NotFound();
+            }
+
+            return employe;
+        }
+
+        // PUT: api/Employes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmploye(int id, Employe employe)
+        {
+            if (id != employe.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(employe).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Employes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Employe>> PostEmploye(Employe employe)
+        {
+            _context.Employes.Add(employe);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEmploye", new { id = employe.ID }, employe);
+        }
+
+        // DELETE: api/Employes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmploye(int id)
+        {
+            var employe = await _context.Employes.FindAsync(id);
+            if (employe == null)
+            {
+                return NotFound();
+            }
+
+            _context.Employes.Remove(employe);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EmployeExists(int id)
+        {
+            return _context.Employes.Any(e => e.ID == id);
+        }
+    }
+}
