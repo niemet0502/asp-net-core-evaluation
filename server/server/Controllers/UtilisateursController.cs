a40c70adfc1d346716dbda7b6e3e0d8b3949fe7e
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-08-02 21:47:47 +0000

    create UtilisateursController and CRUD MEthods

---
+++ server/server/Controllers/UtilisateursController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using server.Models;
+
+namespace server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UtilisateursController : ControllerBase
+    {
+        private readonly ExamenContext _context;
+
+        public UtilisateursController(ExamenContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Utilisateurs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Utilisateur>>> GetUtilisateurs()
+        {
+            return await _context.Utilisateurs.ToListAsync();
+        }
+
+        // GET: api/Utilisateurs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Utilisateur>> GetUtilisateur(int id)
+        {
+            var utilisateur = await _context.Utilisateurs.FindAsync(id);
+
+            if (utilisateur == null)
+            {
+                return NotFound();
+            }
+
+            return utilisateur;
+        }
+
+        // PUT: api/Utilisateurs/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUtilisateur(int id, Utilisateur utilisateur)
+        {
+            if (id != utilisateur.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(utilisateur).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UtilisateurExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Utilisateurs
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Utilisateur>> PostUtilisateur(Utilisateur utilisateur)
+        {
+            _context.Utilisateurs.Add(utilisateur);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUtilisateur", new { id = utilisateur.ID }, utilisateur);
+        }
+
+        // DELETE: api/Utilisateurs/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUtilisateur(int id)
+        {
+            var utilisateur = await _context.Utilisateurs.FindAsync(id);
+            if (utilisateur == null)
+            {
+                return NotFound();
+            }
+
+            _context.Utilisateurs.Remove(utilisateur);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UtilisateurExists(int id)
+        {
+            return _context.Utilisateurs.Any(e => e.ID == id);
+        }
+    }
+}
