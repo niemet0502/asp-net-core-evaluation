6e12fcb57b7867c91c40865342a6bf998e8de472
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-08-11 22:54:12 +0000

    create routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 import SignIn from "./pages/Auth/SignIn";
+ 
 function App() {
   return (
     <div className="App">
-      <SignIn />
+      <div className="page-wrapper null compact-wrapper">
+        <Navbar />
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/employe" element={<SignIn />} />
+        </Routes>
+      </div>
     </div>
   );
 }
